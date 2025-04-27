3139e84a84358077aad25a94636f82904c096606
luluyan0606 <87313381+luluyan0606@users.noreply.github.com>
2025-04-27 15:21:20 +0800

    Add files via upload

---
+++ 2_run_CCCvelo_demo.py
@@ -0,0 +1,87 @@
+import warnings
+import psutil
+import anndata as ad
+
+warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore", category=RuntimeWarning, module="tkinter")
+
+from models.train_CCCvelo import *
+from models.plot_CCCvelo import *
+from models.preprocess_CCCvelo import *
+from models.evaluation_Metric import *
+from sklearn.metrics import roc_auc_score, average_precision_score
+from sklearn.metrics import roc_curve, precision_recall_curve
+
+# CUDA support
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+warnings.filterwarnings("ignore")
+
+def main(seed, inputDir):
+    # Step 1: Load data
+    adata = ad.read_h5ad(os.path.join(inputDir,'adata_raw.h5ad'))
+    results_path = os.path.join(inputDir, f"Output/")
+    create_directory(results_path)
+
+    files = {
+    'LR_link_file': 'LR_links.csv',
+    'TFTG_link_file': 'TFTG_links.csv',
+    'LRTF_score_file': 'TFLR_score/',
+    }
+
+    paths = {key: os.path.join(inputDir, fname) for key, fname in files.items()}
+    adata = PrepareInputData(adata,**paths)
+    adata = root_cell(adata, select_root='UMAP')
+    # sc.pl.embedding(adata, basis="spatial", color="Cluster", s=6, show=True)
+    adata.uns['Cluster_colors'] = ['#ff7f0e', '#2ca02c', '#1f77b4', '#d62728']
+    torch.save(adata, os.path.join(results_path, "pp_adata.pt"))
+ 
+    print('the shape of TGTF_regulate',adata.varm['TGTF_regulate'].shape)
+    print('the shape of TFLR_signaling_score',adata.obsm['TFLR_signaling_score'].shape)
+
+    # Step 2: Train model
+    data = PrepareData(adata, hidden_dims=[150, 150, 150])
+
+    model = SpatialVelocity(*data, lr=0.005, Lambda=0.01)
+    iteration_adam, loss_adam = model.train(200)
+    plotLossAdam(loss_adam, results_path)
+
+    # Extract and save velocity data
+    adata_copy = adata[:, adata.var['TGs'].astype(bool)]
+    adata_velo = get_raw_velo(adata_copy, model)
+    save_model_and_data(model, adata_velo, results_path)
+
+    plt_path = os.path.join(results_path, "figure/")
+    create_directory(plt_path)
+    # plot_gene_dynamic_v2(adata_velo, model, plt_path)
+    plot_gene_dynamic(adata_velo, model, plt_path)
+
+if __name__ == "__main__":
+    process = psutil.Process(os.getpid())
+    before_memory = process.memory_info().rss / 1024 ** 2  # 转换为 MB
+
+    seed = 3 # Replace with your seed value
+    slide_num=25 
+    torch.manual_seed(seed)  # 设置PyTorch的随机种子
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    random.seed(seed)  # 设置Python内置随机数生成器的种子
+    np.random.seed(seed)  # 设置Numpy的随机种子
+    start_time = time.time()
+
+    base_path = "E:/CCCvelo/apply_in_cortex/"
+    outputDir = os.path.join(base_path, f"Output/bin60_input_with_para{slide_num}/RtoPy_test/")
+
+    main(seed, inputDir=outputDir)  # slide_num: 表示不同MLnet参数下的结果
+
+    after_memory = process.memory_info().rss / 1024 ** 2  # 转换为 MB
+    print(f"内存使用增加了: {after_memory - before_memory} MB")
+
+    end_time = time.time()
+    run_time = (end_time - start_time) / 60
+    print(f"Running time is: {run_time} mins")
+
+
